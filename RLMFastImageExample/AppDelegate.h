eae947648ee0b4176dd099f0f1acb134977cd459
Tim Oliver <me@timoliver.com.au>
2018-12-05 10:51:27 +0900

    Initial Commit

---
+++ RLMFastImageExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RLMFastImageExample
+//
+//  Created by Tim Oliver on 4/12/18.
+//  Copyright © 2018 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RLMFastImageExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RLMFastImageExample
+//
+//  Created by Tim Oliver on 4/12/18.
+//  Copyright © 2018 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
