daa7c75c9e21a21b1cd19206ebf6630e681b869b
Tim Oliver <me@timoliver.com.au>
2018-12-09 20:39:02 +0900

    Added RLMFastImage base class

---
+++ RLMFastImage/RLMFastImage.h
@@ -0,0 +1,17 @@
+//
+//  RLMFastImage.h
+//  RLMFastImageExample
+//
+//  Created by Tim Oliver on 9/12/18.
+//  Copyright © 2018 Tim Oliver. All rights reserved.
+//
+
+#import "RLMObject.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RLMFastImage : RLMObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RLMFastImage/RLMFastImage.m
@@ -0,0 +1,13 @@
+//
+//  RLMFastImage.m
+//  RLMFastImageExample
+//
+//  Created by Tim Oliver on 9/12/18.
+//  Copyright © 2018 Tim Oliver. All rights reserved.
+//
+
+#import "RLMFastImage.h"
+
+@implementation RLMFastImage
+
+@end
